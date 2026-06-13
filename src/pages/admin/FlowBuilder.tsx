3c3e7affce08d9e1873c19ea5630e49bb8f7a907
joaosantoss12 <joao.md.santos2002@gmail.com>
2026-06-13 02:26:10 +0100

    Revert connection handle and edge sizing to original

--- src/pages/admin/FlowBuilder.tsx
+++ src/pages/admin/FlowBuilder.tsx
@@ -302,7 +302,6 @@ function Builder() {
           onNodesDelete={onNodesDelete}
           onDrop={onDrop}
           onDragOver={onDragOver}
-          connectionRadius={45}
           onInit={(instance) => {
             instance.fitView();
             // Start ~2 zoom-out clicks further out than fit (1.2 per click).
